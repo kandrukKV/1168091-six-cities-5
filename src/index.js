a762b01d59b3ab47ec936b96426814edf25d4c4e
Konstantin Kandruk <kandruk@ya.ru>
2020-10-02 13:13:16 +0300

    module2-task1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app";
 
 const Settings = {
-  RENTA_OFFERS_NUMBERS: 313
+  RENTA_OFFERS_NUMBER: 313
 };
 
 ReactDOM.render(
     <App
-      rentaOffersNumber={Settings.RENTA_OFFERS_NUMBERS}
+      rentaOffersNumber={Settings.RENTA_OFFERS_NUMBER}
     />,
     document.querySelector(`#root`)
 );
